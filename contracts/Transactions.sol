a2e1300004479289f6eb064d6f6b9993ff4ee007
David Llop <931684+sembrestels@users.noreply.github.com>
2020-05-05 13:58:30 +0200

    Add dummy role to improve compatibility (fixes #10)

--- contracts/Transactions.sol
+++ contracts/Transactions.sol
@@ -5,6 +5,9 @@ import "@aragon/os/contracts/apps/AragonApp.sol";
 
 contract Transactions is AragonApp {
 
+    // ACL
+    bytes32 constant public DUMMY_ROLE = keccak256("DUMMY_ROLE");
+
     function initialize() public onlyInit {
         initialized();
     }
